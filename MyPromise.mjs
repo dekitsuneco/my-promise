26b049b73607cf1e4244897672b362d8581ba1ce
Akira <47333988+dekitsuneco@users.noreply.github.com>
2022-03-21 00:59:59 +0500

    Implement .then method

--- MyPromise.mjs
+++ MyPromise.mjs
@@ -18,26 +18,42 @@ export class MyPromise {
     if (isFunction(executor)) {
       try {
         executor(
-          this.resolveWith.bind(this),
-          this.rejectWith.bind(this),
+          this.#resolveWith.bind(this),
+          this.#rejectWith.bind(this),
         );
       } catch (error) {
-        this.rejectWith(error);
+        this.#rejectWith(error);
       }
     }
   }
 
-  resolveWith(value) {
+  #resolveWith(value) {
     if (this.#state === STATE.PENDING) {
       this.#state = STATE.FULFILLED;
       this.#value = value;
     }
   }
 
-  rejectWith(reason) {
+  #rejectWith(reason) {
     if (this.#state === STATE.PENDING) {
       this.#state = STATE.REJECTED;
       this.#reason = reason;
     }
+  }
+
+  then(onFulfilledHandler, onRejectedHandler) {
+    const childPromise = new MyPromise();
+    this.#thenHandlersQueue.push(
+      [childPromise, onFulfilledHandler, onRejectedHandler],
+    );
+
+    // If already settled:
+    if (this.#state === STATE.FULFILLED) {
+      this.#propagateWithFulfilled();
+    } else if (this.#state === STATE.REJECTED) {
+      this.#propagateWithRejected();
+    }
+
+    return childPromise;
   }
 }
